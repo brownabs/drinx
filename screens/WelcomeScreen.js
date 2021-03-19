2c0d117bb9a0bccc8efbf8370b12fac9bff5044a
Abbey Brown <abbeybrownsemail@gmail.com>
2021-03-18 22:45:52 -0500

    welcome page complete

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { View, StyleSheet, Text, Image } from "react-native";
-
+import { useFonts } from "expo-font";
 import AppButton from "../components/AppButton";
 import Colors from "../utils/colors";
 import useStatusBar from "../hooks/useStatusBar";
@@ -8,17 +8,34 @@ import useStatusBar from "../hooks/useStatusBar";
 export default function WelcomeScreen({ navigation }) {
   useStatusBar("light-content");
 
+  const [loaded] = useFonts({
+    RyeRegular: require("../assets/fonts/Rye-Regular.ttf"),
+  });
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.logoContainer}>
-        <Image source={require("../assets/flame.png")} style={styles.logo} />
-        <Text style={styles.subtitle}>DRINX</Text>
+        <Image
+          source={require("../assets/lastdrinx.jpg")}
+          style={styles.logo}
+        />
       </View>
+      <Text style={styles.subtitle}>DRINX</Text>
       <View style={styles.buttonContainer}>
-        <AppButton title="Login" onPress={() => navigation.navigate("Login")} />
         <AppButton
+          color="black"
+          style={styles.login}
+          title="Login"
+          onPress={() => navigation.navigate("Login")}
+        />
+        <AppButton
+          color="black"
+          style={styles.login}
           title="Register"
-          color="secondary"
           onPress={() => navigation.navigate("Register")}
         />
       </View>
@@ -39,18 +56,29 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   logo: {
-    width: 125,
-    height: 125,
+    flex: 1,
+    resizeMode: "cover",
   },
   subtitle: {
-    fontSize: 24,
-    fontWeight: "600",
-    paddingVertical: 20,
-    color: Colors.primary,
+    color: "white",
+    width: "100%",
+    fontSize: 100,
+    fontWeight: "bold",
+    textAlign: "center",
+    position: "absolute",
+    top: 100,
+    fontFamily: "RyeRegular",
   },
   buttonContainer: {
     padding: 20,
     paddingBottom: 60,
     width: "100%",
+  },
+  login: {
+    opacity: 0.7,
+    width: "90%",
+    height: "25%",
+    borderRadius: 20,
+    color: "white",
   },
 });
